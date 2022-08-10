eaca594eae698944c2a6fb9a7224cfe99ec24881
jihoon-git <hooon.h@gmail.com>
2022-08-10 15:45:34 +0900

    for문 문제풀이2

--- 03_control/src/edu/kh/control/loop/LoopRun.java
+++ 03_control/src/edu/kh/control/loop/LoopRun.java
@@ -9,6 +9,7 @@ public class LoopRun {
 		
 //		forEx.ex1();
 //		forEx.ex2();
+//		forEx.ex3();
 //		forEx.ex4();
 //		forEx.ex5();
 //		forEx.ex6();
@@ -17,7 +18,13 @@ public class LoopRun {
 //		forEx.ex9();
 //		forEx.ex10();
 //		forEx.ex11();
-		forEx.ex12();
+//		forEx.ex12();
+//		forEx.ex13();
+//		forEx.ex14();
+//		forEx.ex15();
+//		forEx.ex16();
+//		forEx.ex17();
+		forEx.ex18();
 		
 		
 		
---
+++ 03_control/src/edu/kh/control/loop/practice/LoopPractice.java
@@ -0,0 +1,9 @@
+package edu.kh.control.loop.practice;
+
+public class LoopPractice {
+
+	// 실습문제 1~13
+	
+	public void practice1() {}
+	
+}
