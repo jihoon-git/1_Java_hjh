a16f0cf5cb7771cb22763a29dbfd5a5181cb278d
jihoon-git <hooon.h@gmail.com>
2022-08-12 12:49:58 +0900

    배열 예제 풀이

--- 04_array/src/edu/kh/array/ex/ArrayExampleRun.java
+++ 04_array/src/edu/kh/array/ex/ArrayExampleRun.java
@@ -5,7 +5,13 @@ public class ArrayExampleRun {
 	
 		ArrayExample arrayEx = new ArrayExample();
 		
-		arrayEx.ex1();
+//		arrayEx.ex1();
+//		arrayEx.ex2();
+//		arrayEx.ex3();
+//		arrayEx.ex4();
+//		arrayEx.ex5();
+//		arrayEx.ex6();
+		arrayEx.ex7();
 		
 }
 }
