cb1842afdd68c7377f7914841a7c6646c3de08cb
jihoon-git <hooon.h@gmail.com>
2022-08-18 12:50:15 +0900

    OOP(클래스, 필드)

---
+++ 05_oop/src/edu/kh/oop/field/model/vo/Trainee.java
@@ -0,0 +1,28 @@
+package edu.kh.oop.field.model.vo;
+
+public class Trainee { // 훈련생
+
+	public String name; // 이름
+	public String phone; // 휴대폰 번호
+	public String email="aaa"; // 이메일(선택)
+	public int salary; //급여(훈련수당)
+	public static char classRoom = 'A'; //강의장
+	
+	// 명시적 초기필드 선언 시 바로 값 초기화
+	// ex) public String email = "없음";
+	
+	// 초기화 블록 : 객체(instance) 객체의 필드를 초기화 하는 블록
+	// - instance block : static이 없는 필드 값 초기화(생성 시 마다)
+	// - static block : static이 없는 필드 값 초기화(프로그램 실행 시 1회)
+	
+	
+	{
+		email = "없음";
+		salary = 116000;
+	}
+	
+	static { // static block
+		classRoom='A';
+	}
+	
+}
