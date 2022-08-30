6bc051f4690e9092a1eacd332cd44a4febeaae57
jihoon-git <hooon.h@gmail.com>
2022-08-30 16:38:25 +0900

    사원 관리 프로그램 만들기

---
+++ EmployeeManagement/src/edu/kh/emg/run/Run.java
@@ -0,0 +1,13 @@
+package edu.kh.emg.run;
+
+import edu.kh.emg.view.EmployeeView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		EmployeeView empview = new EmployeeView();
+		
+		
+	}
+}
---
+++ EmployeeManagement/src/edu/kh/emg/view/EmployeeView.java
@@ -0,0 +1,13 @@
+package edu.kh.emg.view;
+
+import java.util.Scanner;
+
+import edu.kh.emg.model.vo.Employee;
+
+public class EmployeeView {
+
+	private Scanner sc = new Scanner(System.in);
+	private Employee emp = new Employee();
+	
+	
+}
